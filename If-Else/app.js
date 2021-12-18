ef5830f385ecdb6b510b653daf2cc076afe2fac6
ADITYA964 <adityab110699@gmail.com>
2021-12-18 23:41:14 +0530

    Create app.js

---
+++ If-Else/app.js
@@ -0,0 +1,14 @@
+var totalPrice = 19;
+var shippingCost;
+
+if(totalPrice > 20) {
+  shippingCost = 0
+} else {
+  shippingCost = 5
+}
+
+if(totalPrice <= 20) {
+  shippingCost = 5
+} else {
+  shippingCost = 0
+}
